dbb4ba122730cb3b2c80553d531475ac9dab3e33
Zachary Kincaid <zkincaid@gmail.com>
2026-02-22 13:40:37 -0500

    Initial check-in

---
+++ sp26_hw3_tests.ml
@@ -0,0 +1,39 @@
+(** See the project instructions for more details about test case
+    requirements.
+
+    Add your test case to this list.
+
+    Each test case is a triple of the form:
+    (<filename>,<stdin>,<expected_stdout>)
+
+    - <filename> should name an *.oat file that appears in this directory
+      It should should declare the standard oat entry point:
+
+      int program(int argc, string[] argv)
+
+    - <stdin> should be the string passed as the command-line arguments
+      to the executable generaged by compiling <filename> with
+      the following sequence of commands:
+
+      ./oatc <filename> bin/runtime.c
+      ./a.out <stdin>
+
+    - <expected_stdout> is the string representing the expected result
+      obtained by running the compiled <filename> on <stdin> and
+      concatenating the status code (0-255) returned by the call.
+
+    You can use the command line to compile and run such tests like this:
+
+    > ./oatc sp26_hw3_tests/demo_test.oat bin/runtime.c
+    > ./a.out abc
+    abcabc
+
+    These test cases will be run via Gradedtests.oat_file_test.  For
+    additional examples, see the tests/gradedtests.ml file.
+ *)
+
+let student_tests : (string * string * string) list = [
+    (* provided demo example *)
+    ("demo_test.oat", "abc", "abcabc0");
+    ("demo_test.oat", "defg", "defgdefg0");
+  ]
